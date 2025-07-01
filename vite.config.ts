68fa42166478abd3f34b5e748c02e2753f22f85b
Holden Hu <sedationh@gmail.com>
2025-07-01 15:29:08 +0800

    add pwa feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import react from "@vitejs/plugin-react-swc"
+import { VitePWA } from "vite-plugin-pwa"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 })

8646c322a265ba95fee7ffd29c25a4efca48b661
Holden Hu <sedationh@gmail.com>
2025-07-03 11:21:16 +0800

    refactor index.html and update package.json dependencies; remove Radix UI themes, add @types/node, and update favicon; enhance TypeScript configuration with baseUrl and paths; clean up Vite config and App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button, Flex, Text } from "@radix-ui/themes"
-
 function App() {
   return (
-    <Flex align="center" justify="center" gap="2" className="h-screen">
-      <Text>Hello from Radix Themes :)</Text>
-      <Button>Let's go</Button>
-    </Flex>
+    <div className="flex flex-col h-screen items-center justify-center gap-1">
+      <h1 className="text-4xl font-bold text-red-300">Hello Holden</h1>
+      <img src="/favicon.svg" alt="favicon" className="w-30 h-30" />
+    </div>
   )
 }
 

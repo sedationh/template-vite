06c59549a4c30b3f097c343b7d885b64e2f29471
Holden Hu <sedationh@gmail.com>
2025-07-03 11:59:57 +0800

    update VSCode settings for import module specifier preferences; enhance App component by adding Apple icon to Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button"
+import { Apple } from "lucide-react"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <Button>
+        Click me <Apple />
+      </Button>
     </div>
   )
 }

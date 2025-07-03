e1a457fbae5ab24a91df321f31ab8abf47168adb
Holden Hu <sedationh@gmail.com>
2025-07-03 11:45:49 +0800

    update package.json and pnpm-lock.yaml with new dependencies; refactor App component to use Button from UI library; enhance CSS with custom theme variables and animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <div className="flex flex-col h-screen items-center justify-center gap-1">
-      <h1 className="text-4xl font-bold text-red-300">Hello Holden</h1>
-      <img src="/favicon.svg" alt="favicon" className="w-30 h-30" />
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   )
 }
